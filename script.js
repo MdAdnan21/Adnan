67b9280247cf4e62846b9af4081d11a48d7b3704
MdAdnan21 <121079748+MdAdnan21@users.noreply.github.com>
2024-03-02 16:21:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var tl = gsap.timeline();
+
+gsap.from("#nav img, #nav h3, #nav h4, #nav button", {
+  y: -100,
+  duration: 0.5,
+  opacity: 0,
+  stagger: 0.2,
+});
+tl.from("#main h1", {
+  y: 120,
+  opacity: 0,
+  stagger: 0.3,
+});
+tl.from("#main>img", {
+  scale: 0,
+  opacity: 0,
+  stagger: 0.3,
+});
+tl.from("h5", {
+  scale: 0,
+  opacity: 0,
+});
+tl.to("h5", {
+  y: 40,
+  repeat: -1,
+  duration: 0.7,
+  yoyo: true,
+});
